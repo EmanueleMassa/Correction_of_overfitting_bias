dbd148726f1fbceeec7857fe9e79b41b442e7aec
EmanueleMassa <107191995+EmanueleMassa@users.noreply.github.com>
2023-07-11 14:39:50 +0200

    Update ideal_solver_logit.py

--- ideal_solver_logit.py
+++ ideal_solver_logit.py
@@ -2,11 +2,12 @@ import numpy as np
 import matplotlib.pyplot as plt 
 import numpy.random as rnd 
 
-GH = np.loadtxt('GH.txt')
+GH = np.loadtxt('GH.txt') #load the list of point and weights for Hermite Gauss quadrature
 
 gp=  np.sqrt(2)*GH[:,0]     #gaussian points
 gw = GH[:,1]/np.sqrt(np.pi) #gaussian weights           
 
+#define the implicit function $\chi$ as in the main text of the article
 def chi(x,y,mu):
     err = 1.0
     z0 = (x-mu*y)/(1.0+mu)
@@ -23,10 +24,13 @@ def chi(x,y,mu):
             return np.inf
     return z
 
+#define the likelihood of a bernoulli model, with exponential parametrization
 def f(x,y):
 	return np.exp(-y*x)/(2*np.cosh(x))
 
+#solve the RS equations, given the true data generating process (known in simulations)
 def RS_ideal_solver(zeta,beta_true,phi_true):
+#initialization
     tau0 = zeta
     w0 = beta_true
     v0 = zeta
@@ -39,11 +43,12 @@ def RS_ideal_solver(zeta,beta_true,phi_true):
 
     err = 1.0
     its = 0
+#start of the self consistent loop 
     while (err>1.0e-13):
-        M = np.add.outer(v0*gp,w0*gp) + phi0
+        M = np.add.outer(v0*gp,w0*gp) + phi0 			#matrix argument for function chi 
         chi1 = chi(M,1,tau0)
         chi0 = chi(M,-1,tau0)
-        w1 = (gw)@(chi1 + phi0)@(gp*gw*f1) + (gw)@(chi0 +phi0)@(gp*gw*f0)
+        w1 = (gw)@(chi1 + phi0)@(gp*gw*f1) + (gw)@(chi0 +phi0)@(gp*gw*f0) #w = kappa*theta_0
         tau1 = zeta/((gw)@(1.0/(tau0 + np.cosh(chi1)**2))@(gw*f1) + (gw)@(1.0/(tau0 + np.cosh(chi0)**2))@(gw*f0))
         v1 = (gw)@((tau0 + tau0*np.tanh(chi1))**2)@(gw*f1) + (gw)@((-tau0 +tau0*np.tanh(chi0))**2)@(gw*f0)
         v1 = np.sqrt(v1/zeta)
