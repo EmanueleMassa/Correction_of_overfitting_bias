3585243c1269312fc12bd89202fe89dedf8c1052
EmanueleMassa <107191995+EmanueleMassa@users.noreply.github.com>
2023-09-04 10:46:49 +0200

    Update data_generator_weibull.py

--- data_generator_weibull.py
+++ data_generator_weibull.py
@@ -76,39 +76,45 @@ def normal(x,mu,sigma):
 #beta1
 plt.figure()
 plt.title(r"$n=$"+str(n)+r" $\zeta=$"+'{:.2f}'.format(p/n)+r" $\theta_0=$"+str(beta0[0]))
-plt.hist(Beta_ml[:,0],density=True,color='grey')
-x = np.linspace(min(Beta_ml[:,0]),max(Beta_ml[:,0]),10000)
-plt.plot(x,normal(x,rho*beta0[0],v/np.sqrt(p)))
-plt.xlabel(r"$\hat{\mathbf{\beta}}_n'\mathbf{e}_1$")
-plt.savefig('beta1.png')
+plt.hist(Beta_ml[:,0],density=True,color='lightgrey',label = r"$\mathbf{e}_1'\hat{\mathbf{\beta}}_n$")
+beta =Beta_ml[:,0]/rho
+plt.hist(beta,density=True,color='grey',label = r"$\mathbf{e}_1'\tilde{\mathbf{\beta}}_n$")
+x = np.linspace(min(beta),max(beta),10000)
+plt.plot(x,normal(x,beta0[0],v/(rho*np.sqrt(p))),'k-')
+plt.axvline(x= beta0[0],color = 'black', linestyle = '-.')
+plt.xlabel(r"$\beta_1$")
+plt.savefig('betasim1.zeta'+'{:.2f}'.format(p/n)+'.png')
 
 #beta3
 plt.figure()
 plt.title(r"$n=$"+str(n)+r" $\zeta=$"+'{:.2f}'.format(p/n)+r" $\theta_0=$"+str(beta0[0]))
-plt.hist(Beta_ml[:,p-1],density=True, color='grey')
-x = np.linspace(min(Beta_ml[:,2]),max(Beta_ml[:,2]),10000)
-plt.plot(x,normal(x,rho*beta0[2],v/np.sqrt(p)))
-plt.xlabel(r"$\hat{\mathbf{\beta}}_n'\mathbf{e}_3$")
-plt.savefig('betap.png')
+plt.hist(Beta_ml[:,1],density=True, color='lightgrey',label = r"$\mathbf{e}_2'\hat{\mathbf{\beta}}_n$")
+beta =Beta_ml[:,1]/rho
+plt.hist(beta,density=True,color='grey',label = r"$\mathbf{e}_2'\tilde{\mathbf{\beta}}_n$")
+x = np.linspace(min(beta),max(beta),10000)
+plt.plot(x,normal(x,beta0[1],v/(rho*np.sqrt(p))),'k-')
+plt.axvline(x= beta0[1],color = 'black', linestyle = '-.')
+plt.xlabel(r"$\beta_2$")
+plt.savefig('betasim2.zeta'+'{:.2f}'.format(p/n)+'.png')
 
-#intercept (phi)
+#phi
 plt.figure()
 plt.title(r"$n=$"+str(n)+r" $\zeta=$"+'{:.2f}'.format(p/n)+r" $\theta_0=$"+str(beta0[0]))
 plt.hist(Phi_ml,density=True,color = 'lightgrey',label = r"$\hat{\phi}_n$")
-plt.hist((Phi_ml-phi)/rho,density=True,color = 'grey',label = r"$\hat{\phi}^{\sim}_n$")
+plt.hist((Phi_ml-phi)/rho,density=True,color = 'grey',label = r"$\tilde{\phi}_n$")
 plt.axvline(x= phi0,color = 'black', linestyle = '-.')
-plt.legend()
-plt.savefig('phi.png')
+plt.xlabel(r"$\phi$")
+plt.savefig('phi.zeta'+'{:.2f}'.format(p/n)+'.png')
 
-
-# power parameter (rho)
+# rho
 plt.figure()
 plt.title(r"$n=$"+str(n)+r" $\zeta=$"+'{:.2f}'.format(p/n)+r" $\theta_0=$"+str(beta0[0]))
 plt.hist(Rho_ml,density=True,color = 'lightgrey',label = r"$\hat{\rho}_n$")
-plt.hist(Rho_ml/rho,density=True,color = 'grey',label = r"$\hat{\rho}^{\sim}_n$")
+plt.hist(Rho_ml/rho,density=True,color = 'grey',label = r"$\tilde{\rho}_n$")
 plt.axvline(x= rho0,color = 'black', linestyle = '-.')
-plt.legend()
-plt.savefig('rho.png')
+plt.xlabel(r"$\sigma$")
+plt.savefig('rho.zeta'+'{:.2f}'.format(p/n)+'.png')
+
 plt.show()
 
     
